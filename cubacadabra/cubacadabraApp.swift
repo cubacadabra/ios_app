5927c46841dde4db54c2ea51b99b82284223c964
aa <oneone@gmail.com>
2026-09-01 12:56:03 -0700

    first

---
+++ cubacadabra/cubacadabraApp.swift
@@ -0,0 +1,17 @@
+//
+//  cubacadabraApp.swift
+//  cubacadabra
+//
+//  Created by aa on 9/1/26.
+//
+
+import SwiftUI
+
+@main
+struct cubacadabraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
