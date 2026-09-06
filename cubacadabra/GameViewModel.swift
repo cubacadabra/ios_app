e3e33b892f49aa6e49fd043c42f6e6d3896e7255
aa <oneone@gmail.com>
2026-09-06 09:51:26 -0700

    more color fixes

--- cubacadabra/GameViewModel.swift
+++ cubacadabra/GameViewModel.swift
@@ -762,10 +762,20 @@ final class GameViewModel: ObservableObject {
             username = serverUsername
             engine?.setUsername(serverUsername)
         }
-        if let appearance = event.appearance,
-           let data = try? JSONEncoder().encode(appearance),
-           let source = String(data: data, encoding: .utf8) {
-            _ = engine?.setLocalAppearance(source)
+        if let appearance = event.appearance {
+            let localAppearance = WorldAppearance(
+                version: appearance.version,
+                body: appearance.body,
+                face: appearance.face,
+                outfit: appearance.outfit,
+                equipment: appearance.equipment,
+                colors: appearance.colors,
+                revision: max(appearance.revision ?? 0, (engine?.appearanceRevision ?? 0) + 1)
+            )
+            if let data = try? JSONEncoder().encode(localAppearance),
+               let source = String(data: data, encoding: .utf8) {
+                _ = engine?.setLocalAppearance(source)
+            }
         }
     }
 
